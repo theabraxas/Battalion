040691432000e5529ee932b4965adc2225c61418
Abraxas <stewart.olson@gmail.com>
2016-11-19 13:19:46 -0800

    Added XML output for each nmap scan. (#32)

--- domain-scan/scripts/nmap-basic.sh
+++ domain-scan/scripts/nmap-basic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SCAN_DIRECTORY="${1}"
+OUTPUT_DIRECTORY="${1}"
 
 while read SUBDOMAIN
 do
-    (>&2 echo -e "\t+ Executing light Nmap scan for domain [${SUBDOMAIN}]")
-    nmap -T4 --open -F -Pn ${SUBDOMAIN} > "${SCAN_DIRECTORY}/${SUBDOMAIN}.txt"
+    (>&2 echo -e "\t+ Executing light Nmap scan for domain [$SUBDOMAIN]")
+    nmap -T4 --open -F -Pn $SUBDOMAIN -oX "$OUTPUT_DIRECTORY/$SUBDOMAIN.xml" > "$OUTPUT_DIRECTORY/$SUBDOMAIN.txt"
 done < /dev/stdin
