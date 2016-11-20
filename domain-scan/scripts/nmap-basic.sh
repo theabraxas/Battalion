f467fb5e85fde635bdcf70c0d39a7e3e3f7b7433
Abraxas <stewart.olson@gmail.com>
2016-11-19 19:49:23 -0800

    Update nmap-basic.sh

--- domain-scan/scripts/nmap-basic.sh
+++ domain-scan/scripts/nmap-basic.sh
@@ -5,5 +5,5 @@ OUTPUT_DIRECTORY="${1}"
 while read SUBDOMAIN
 do
     (>&2 echo -e "\t+ Executing light Nmap scan for domain [$SUBDOMAIN]")
-    nmap -T4 --open -F -Pn $SUBDOMAIN -oX "$OUTPUT_DIRECTORY/$SUBDOMAIN.xml" > "$OUTPUT_DIRECTORY/$SUBDOMAIN.txt"
+    nmap -T4 --open -F -v -Pn $SUBDOMAIN -oX "$OUTPUT_DIRECTORY/$SUBDOMAIN.xml" > "$OUTPUT_DIRECTORY/$SUBDOMAIN.txt"
 done < /dev/stdin
