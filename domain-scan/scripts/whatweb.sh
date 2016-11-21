07f09a1d43478245e562a014b622b5153c312f32
theabraxas <stewart.olson@gmail.com>
2016-11-21 00:25:53 -0500

    Added JSON output for WhatWeb

--- domain-scan/scripts/whatweb.sh
+++ domain-scan/scripts/whatweb.sh
@@ -6,6 +6,6 @@ SUBDOMAIN_FILE="${2}"
 while read SUBDOMAIN; do
     # For each subdomain, execute WhatWeb and dump the raw results to a file.
     OUTPUT="${SCAN_DIRECTORY}/${SUBDOMAIN}.txt"
-    ${BATTALION_WHATWEB_HOME}/whatweb --color=never --no-errors -a1 -v "${SUBDOMAIN}" > "${OUTPUT}"
+    JSONOUTPUT="${SCAN_DIRECTORY}/${SUBDOMAIN}.json"
+    ${BATTALION_WHATWEB_HOME}/whatweb --color=never --no-errors -a1 -v "${SUBDOMAIN}" --log-json="${JSONOUTPUT}" > "${OUTPUT}"
 done <${SUBDOMAIN_FILE}
-
